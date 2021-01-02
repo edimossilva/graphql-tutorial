8245504942f26496e5ea1c3c31b15eebfcdab3a3
Edimo Sousa <edimossilva@gmail.com>
2021-01-02 17:59:17 +0200

    create user

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,24 @@
+module Mutations
+  class CreateUser < BaseMutation
+    # often we will need input types for specific mutation
+    # in those cases we can define those input types in the mutation class itself
+    class AuthProviderSignupData < Types::BaseInputObject
+      argument :credentials, Types::AuthProviderCredentialsInput, required: false
+    end
+
+    argument :name, String, required: true
+    argument :auth_provider, AuthProviderSignupData, required: false
+
+    type Types::UserType
+
+    def resolve(name: nil, auth_provider: nil)
+      puts name
+      puts auth_provider
+      User.create!(
+        name: name,
+        email: auth_provider&.[](:credentials)&.[](:email),
+        password: auth_provider&.[](:credentials)&.[](:password)
+      )
+    end
+  end
+end
